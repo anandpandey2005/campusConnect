ac8f51149c524e1afeac9ced2d03623047a26ac4
ANAND PANDEY <anandpandey20005@gmail.com>
2025-10-27 23:57:00 +0530

    client setup -axios -tailwind -react-router-dom - zustand -prettier

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { NotFound } from "./handlers/pageHandler.js";
+function App() {
+
+
+  return <>
+  <h1>hello page gettign started </h1>
+  <NotFound/>
+  </>;
+}
+
+export default App;
